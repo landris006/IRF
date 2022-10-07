8d9037567a699b5e9026b11897e71336beda1c07
landris006 <laczko.andris007@gmail.com>
2022-10-07 10:23:47 +0200

    excel init

--- excel_export_pdiw2h/excel_export_pdiw2h/Form1.cs
+++ excel_export_pdiw2h/excel_export_pdiw2h/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace excel_export_pdiw2h
 {
@@ -14,15 +16,46 @@ namespace excel_export_pdiw2h
     {
         private List<Flat> flats;
         private RealEstateEntities realEstateEntities = new RealEstateEntities();
+        private Excel.Application xlApp;
+        private Excel.Workbook xlWorkBook;
+        private Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
+            loadData();
+
+            createExcel();
         }
 
         private void loadData()
         {
             flats = realEstateEntities.Flat.ToList();
+        }
+
+        private void createExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWorkBook = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWorkBook.ActiveSheet;
+
+                // createTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = string.Format($"Error: {ex.Message}, /nLine: {ex.Source}");
+                MessageBox.Show(errorMessage, "Error");
+
+                xlWorkBook.Close();
+                xlApp.Quit();
+                xlWorkBook = null;
+                xlApp = null;
+            }
         }
     }
 }
