e2c74c0d920d4dfc6908dbbbd4d7fdb8949575a3
landris006 <laczko.andris007@gmail.com>
2022-10-07 10:13:32 +0200

    orm

--- excel_export_pdiw2h/excel_export_pdiw2h/Form1.cs
+++ excel_export_pdiw2h/excel_export_pdiw2h/Form1.cs
@@ -12,9 +12,17 @@ namespace excel_export_pdiw2h
 {
     public partial class Form1 : Form
     {
+        private List<Flat> flats;
+        private RealEstateEntities realEstateEntities = new RealEstateEntities();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loadData()
+        {
+            flats = realEstateEntities.Flat.ToList();
         }
     }
 }
